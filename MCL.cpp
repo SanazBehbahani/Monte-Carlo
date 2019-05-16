eec055905951eea597a3051e376d801c686ef810
Sanaz Behbahani <sanaz.behbahani@gmail.com>
2019-05-16 14:06:12 -0700

    simulate noise

--- MCL.cpp
+++ MCL.cpp
@@ -230,21 +230,16 @@ int main()
     // Instantiating a robot object from the Robot class
     Robot myrobot;
 
-    // TODO: Set robot new position to x=10.0, y=10.0 and orientation=0
-    // Fill in the position and orientation values in myrobot.set() function
-    myrobot.set(10.0, 10.0, 0.0);
-
-    // Printing out the new robot position and orientation
-    cout << myrobot.show_pose() << endl;
-
-    // TODO: Rotate the robot by PI/2.0 and then move him forward by 10.0
-    // Use M_PI for the pi value
-    myrobot.move(M_PI/2.0, 10.0);
-
-    // TODO: Print out the new robot position and orientation
-    cout << myrobot.show_pose() << endl;
-
-    // Printing the distance from the robot toward the eight landmarks
+    // TODO: Simulate Noise
+    // Forward Noise=5.0, Turn Noise=0.1,Sense Noise=5.0
+    myrobot.set_noise(5.0, 0.1, 5.0);
+    
+    myrobot.set(30.0, 50.0, M_PI / 2.0);
+    // cout << myrobot.show_pose() << endl;
+    myrobot.move(-M_PI / 2.0, 15.0);
+    // cout << myrobot.show_pose() << endl;
+    cout << myrobot.read_sensors() << endl;
+    myrobot.move(-M_PI / 2.0, 10.0);
     cout << myrobot.read_sensors() << endl;
 
     return 0;
